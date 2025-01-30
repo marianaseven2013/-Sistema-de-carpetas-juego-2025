9fc29b7b3d4b97daf84e28da77d64b20c2c9aa38
genesis <gmluch@scl.edu.gt>
2025-01-30 13:40:38 -0600

    Sistema de carpetas y su estuctura

---
+++ index.js
@@ -0,0 +1,30 @@
+let DOM = document.querySelector("#root");
+
+//DOM.innerHTML = "Hola mundo";
+let contenedor = document.createElement('div');
+contenedor.className = "connt"
+
+
+let d1Header = document.createElement('div');
+d1Header.className = "d1-header"
+contenedor.appendChild(d1Header);
+
+
+let d2Progreso = document.createElement('div');
+d2Progreso.className = "d2-progreso"
+contenedor.appendChild(d2Progreso);
+
+let d3Tablero = document.createElement('div');
+d3Tablero.className = "d3-tablero"
+contenedor.appendChild(d3Tablero);
+
+let d4Footer = document.createElement('div');
+d4Footer.className = "d4-footeer"
+contenedor.appendChild(d4Footer);
+//Esto para crear un div
+
+
+
+
+DOM.appendChild(contenedor);
+ //Para que aparezca o se cree el div es decir en este caso este es el contenedor de todos los divs
